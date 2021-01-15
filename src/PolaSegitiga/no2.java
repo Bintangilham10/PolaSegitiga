c4d1690772039084e4342a02daa83d15380d77e5
Bintangilham10 <bintang.ilham1401@gmail.com>
2021-01-15 08:09:29 +0700

    PolaSegitiga

---
+++ src/PolaSegitiga/no2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bintang.Ilham
+ */
+public class no2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //deklarasi variabel//
+        int a = 4;
+        
+        //rumus perulangan pola segitiga siku-siku ke kiri//
+        for (int b = 1; b <= a; b++){
+            for (int c = 1; c <= b; c++){
+                
+                //menampilkan output hasil//
+                System.out.print("@");
+            }
+            System.out.println(); 
+        }
+        
+    }
+    
+}
